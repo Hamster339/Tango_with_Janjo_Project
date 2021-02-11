9cf07f1170d02ad86731ca3cf439c7b02b8d3206
Hamish Philip <2464927p@student.gla.ac.uk>
2021-02-11 17:43:20 +0000

    Edn of chapter9 login and out

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -121,11 +121,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Media files
-
 MEDIA_URL = "/media/"
 MEDIA_ROOT = MEDIA_DIR
+
+# login
+LOGIN_URL = 'rango:login'
+
